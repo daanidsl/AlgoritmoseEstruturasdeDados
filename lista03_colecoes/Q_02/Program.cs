34bad05d946d895c26d66a3a0a6a0fdcb931cf36
Daniela da Silva Lopes <80592910+daanidsl@users.noreply.github.com>
2024-10-08 21:00:54 -0300

    Add files via upload

---
+++ lista03_colecoes/Q_02/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<double> notacao = new Stack<double>();
+
+            Console.WriteLine("Informe uma expressao:");
+            string expressao = Console.ReadLine();
+
+            char letra;
+            double v1, v2, resultado = 0;
+
+            for(int i = 0; i < expressao.Length; i++)
+            {
+                letra = expressao[i];
+
+                if (letra != '-' && letra != '+' && letra != '/' && letra != '*')
+                {
+                    double num = (double)Char.GetNumericValue(letra);
+                    notacao.Push(num);
+                }
+                else
+                {
+                    v1 = notacao.Pop();
+                    v2 = notacao.Pop();
+
+                    if (letra == '-')
+                    {
+                        resultado = v2 - v1;
+                        notacao.Push(resultado);
+                    }
+                    else if (letra == '+')
+                    {
+                        resultado = v2 + v1;
+                        notacao.Push(resultado);
+                    }
+                    else if (letra == '/')
+                    {
+                        resultado = v2 / v1;
+                        notacao.Push(resultado);
+                    }
+                    else if (letra == '*')
+                    {
+                        resultado = v2 * v1;
+                        notacao.Push(resultado);
+                    }
+                }
+            }
+            Console.Write("Resultado: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lista03_colecoes/Q_04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> decolagem = new Queue<string>();
+            int opcao;
+
+            do
+            {
+                Console.Write("1) Exibir quantidade de avioes\n2) Autorizar decolagem\n3) Adicionar aviao\n4) Listar fila\n5) Exibir primeiro da fila\n6) Sair\n");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Quantidade de avioes: " + decolagem.Count);
+                        break;
+                    case 2:
+                        Console.WriteLine("O aviao " + decolagem.Peek() + " decolou");
+                        decolagem.Dequeue();
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o identificador do aviao:");
+                        string id = Console.ReadLine();
+                        decolagem.Enqueue(id);
+                        break;
+                    case 4:
+                        foreach (string item in decolagem)
+                            Console.WriteLine(item);
+                        break;
+                    case 5:
+                        Console.WriteLine("Primeiro aviao da fila: " + decolagem.Peek());
+                        break;
+                    case 6:
+                        Console.WriteLine("Fim");
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcao != 6);
+        }
+    }
+}
